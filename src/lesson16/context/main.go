346315d772ae63330b6a80841d5743ec17580153
Shogo Wada <aiz4338150@gmail.com>
2023-01-08 00:34:27 +0900

    finish context

---
+++ src/lesson16/context/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+// context
+
+func logProcess(ctx context.Context, ch chan string) {
+	fmt.Println("開始")
+	time.Sleep(2 * time.Second)
+	fmt.Println("終了")
+	ch <- "実行結果"
+
+}
+
+func main() {
+	ch := make(chan string)
+
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+
+	defer cancel()
+
+	go logProcess(ctx, ch)
+
+L:
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println("##########Error##########")
+			fmt.Println(ctx.Err())
+			break L
+		case s := <- ch:
+			fmt.Println(s)
+			fmt.Println("success")
+			break L
+		}
+
+	}
+
+	fmt.Println("ループ抜けた")
+
+}
+
