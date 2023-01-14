f7954012345c42af1a824f39482a381265edbb2c
Shogo Wada <aiz4338150@gmail.com>
2023-01-14 23:56:17 +0900

    add updating and deleting users data

--- src/lesson18/todo_app/main.go
+++ src/lesson18/todo_app/main.go
@@ -16,12 +16,26 @@ func main() {
 	*/
 
 	fmt.Println(models.Db)
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.Password = "testtest"
+		fmt.Println(u)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
+		u.CreateUser()
+	*/
 
-	u.CreateUser()
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
