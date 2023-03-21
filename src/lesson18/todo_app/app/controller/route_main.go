8d00352c7ba17545b773acf65d7902cc8a829f3b
Shogo Wada <aiz4338150@gmail.com>
2023-03-21 12:58:27 +0900

    create todo feature

--- src/lesson18/todo_app/app/controller/route_main.go
+++ src/lesson18/todo_app/app/controller/route_main.go
@@ -28,3 +28,33 @@ func index(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
 }
+
+func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := sess.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		if err := user.CreateTodo(content); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", http.StatusFound)
+	}
+}
